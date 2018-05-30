1d9d274427fe3d620dd226ce323505a52cee563b
momizi <37651124+MaKuELe@users.noreply.github.com>
2018-05-30 23:17:20 +0900

    game_mainの処理にコメントを追加。

--- marubatu.py
+++ marubatu.py
@@ -35,20 +35,29 @@ def game_main():
     place=['1','2','3','4','5','6','7','8',"9"]
     counter =0
     while counter<9:
+        #現状の増すの状態を表示
         print_list(place)
 
+        #○を記すか、×を記すか入力してもらう
         write_val = input("○:1 or ×:2:")
 
+        #マス目を指定してもらう
+        #現状0だったら終了。
         place_val = input("to :")
         if place_val[0]=="0":
             exit()
-                    
+
+        #マス目を検索
+        # 範囲外のマスを指定された場合ValueErrorで処理  
         try:
             target_index=place.index(place_val[0])
         except ValueError:
             print("指定されたマスが見つかりませんでした。")
-            continue
+
         try:
+            #入力値を ○ × に変換　
+            #指定されたマスの値を削除
+            #削除した位置に○か×を挿入
             if write_val[0] =="1":
                 write="○"    
             elif write_val[0] =="2":
@@ -58,11 +67,14 @@ def game_main():
             place.pop(target_index)
             place.insert(target_index,write)
         except :
+            #とりあえず例外処理
             print("EXEPTIN")
             continue
 
+        #勝利判定
         if "fin"==winner(place):
             break
         
+        #入力回数の加算
         counter+=1
     return 1
