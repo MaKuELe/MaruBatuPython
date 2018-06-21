f3824a131c637bfa9c5a6dbdfd8383d536408157
momizi <37651124+MaKuELe@users.noreply.github.com>
2018-06-21 20:56:21 +0900

    marubatu_gui.py:不要なクラス、変数を削除。

--- marubatu_gui.py
+++ marubatu_gui.py
@@ -2,55 +2,13 @@ import wx
 import marubatu
   
 class MarubatuApp(wx.App):
-    
-    #game
-    #STRA="CCC"
+ 
     def OnInit(self): 
-        self.STRA="ABC"
         self.game=marubatu.marubatu_data()
         self.main_flame=MarubatuFrame()
         self.main_flame.Show()
         return True
 
-    #=marubatu.marubatu_data()
-    def setSTRA(self):
-        print ("%s"%self.STRA)
-        self.STRA="BBB"
-        print ("%s"%self.STRA)
-
-    def GetGameData2(self):
-        return game
-  
-class CalcFrame(wx.Frame):
-    """
-    フレームを継承したトップレベルウィンドウクラス
-    """
-  
-    def __init__(self):
-          
-        super().__init__(None, wx.ID_ANY, '電卓レイアウト', size=(180, 400))
-  
-        # ステータスバーの初期化
-        self.CreateStatusBar()
-        self.SetStatusText('https://www.python-izm.com/')
-        self.GetStatusBar().SetBackgroundColour(None)
-  
-        # メニューバーの初期化
-        #self.SetMenuBar(CalcMenu())
-          
-        # 本体部分の構築
-        root_panel = wx.Panel(self, wx.ID_ANY)
-  
-        text_panel = TextPanel(root_panel)
-        #cmdbutton_panel = CommandButtonPanel(root_panel)
-        calcbutton_panel = CalcButtonPanel(root_panel)
-  
-        root_layout = wx.BoxSizer(wx.VERTICAL)
-        root_layout.Add(text_panel, 0, wx.GROW | wx.ALL, border=10)
-       # root_layout.Add(cmdbutton_panel, 0, wx.GROW | wx.LEFT | wx.RIGHT, border=20)
-        root_layout.Add(calcbutton_panel, 0, wx.GROW | wx.ALL, border=10)
-        root_panel.SetSizer(root_layout)
-        root_layout.Fit(root_panel)
 
 class MarubatuFrame(wx.Frame):
     """
@@ -59,14 +17,10 @@ class MarubatuFrame(wx.Frame):
   
     def __init__(self):
           
-        super().__init__(None, wx.ID_ANY, '○×ゲーム', size=(180, 400))
-  
-        # ステータスバーの初期化
-        #self.CreateStatusBar()
-        #self.GetStatusBar().SetBackgroundColour(None)
-  
+        super().__init__(None, wx.ID_ANY, '○×ゲーム', size=(180, 200))
+    
         # メニューバーの初期化
-        #self.SetMenuBar(CalcMenu())
+        self.SetMenuBar(MarubatuMenu())
           
         # 本体部分の構築
         root_panel = wx.Panel(self, wx.ID_ANY)
@@ -83,7 +37,7 @@ class MarubatuFrame(wx.Frame):
       #  root_layout.Fit(root_panel)
   
 
-class CalcMenu(wx.MenuBar):
+class MarubatuMenu(wx.MenuBar):
     """
     CalcFrameにセットするメニューバークラス
     """
@@ -92,15 +46,12 @@ class CalcMenu(wx.MenuBar):
   
         super().__init__()
           
-        menu_file = wx.Menu()
-        menu_file.Append(wx.ID_ANY, '保存')
-        menu_file.Append(wx.ID_ANY, '終了')
-        menu_edit = wx.Menu()
-        menu_edit.Append(wx.ID_ANY, 'コピー')
-        menu_edit.Append(wx.ID_ANY, 'ペースト')
+        menu_manage = wx.Menu()
+        menu_manage.Append(wx.ID_ANY, 'リセット')
+        menu_manage.Append(wx.ID_ANY, '終了')
           
-        self.Append(menu_file, 'ファイル')
-        self.Append(menu_edit, '編集')
+        self.Append(menu_manage, '管理')
+
   
   
 class TextPanel(wx.Panel):
@@ -181,10 +132,6 @@ class CalcButtonPanel(wx.Panel):
       
         super().__init__(parent, wx.ID_ANY)
   
-        button_collection = ('1','2','3'
-        ,'4','5','6'
-        ,'7','8','9')
-  
         layout = wx.GridSizer(3, 3,0,0)
         
         for i in range(0,9):
@@ -202,7 +149,4 @@ if __name__ == '__main__':
   
     # カスタムフレームを初期化してアプリケーションを開始
     application = MarubatuApp()
-   # frame = MarubatuFrame()
- #   application.main_flame.Show()
-#    frame.Show()
     application.MainLoop()
